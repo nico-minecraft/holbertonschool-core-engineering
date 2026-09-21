5f73a8b94b9fb083dea985546f011f40e80f80ec
Nico <nico.ls.asdf@gmail.com>
2026-09-20 20:19:54 -0600

    Add script 4

---
+++ python_fundamentals/exception_handling/raise_exception.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that defines a function raising a TypeError."""
+
+
+def raise_exception():
+    """Raise a TypeError."""
+    raise TypeError("This is a TypeError")
