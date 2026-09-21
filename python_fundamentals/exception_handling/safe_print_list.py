86e1ffd2023f80e2e110fa71095446c0b3c5541c
Nico <nico.ls.asdf@gmail.com>
2026-09-20 20:12:06 -0600

    Add script 0

---
+++ python_fundamentals/exception_handling/safe_print_list.py
@@ -0,0 +1,10 @@
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for index in range(x):
+            print(my_list[index], end="")
+            count += 1
+    except IndexError:
+        pass
+    print()
+    return count
