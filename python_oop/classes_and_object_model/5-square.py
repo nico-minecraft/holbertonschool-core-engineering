be8fd28c9941bc8452365a3c0b9a5ba2abfaa2ac
Nico <nico.ls.asdf@gmail.com>
2026-09-20 20:38:10 -0600

    Add script 5

---
+++ python_oop/classes_and_object_model/5-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""Module that defines a Square class with a my_print method."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): the size of the square. Defaults to 0.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value (int): the new size of the square.
+
+        Raises:
+            TypeError: if value is not an integer.
+            ValueError: if value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Print the square with the character #.
+
+        If size is 0, print an empty line.
+        """
+        if self.__size == 0:
+            print()
+            return
+        for _ in range(self.__size):
+            print("#" * self.__size)
